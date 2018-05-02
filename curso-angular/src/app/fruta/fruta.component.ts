e8b8e83b55387aa6629e360ce0c42ae7c5a576ef
DFCGamerYT <DFCGamerYT@gmail.com>
2018-05-02 15:35:00 -0500

    Propiedades y tipos de datos

--- curso-angular/src/app/fruta/fruta.component.ts
+++ curso-angular/src/app/fruta/fruta.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 export class FrutaComponent{
     public nombre_componente = 'Componente de Fruta';
     public listado_frutas = 'Naranja, Manzana, Pera y Sandia';
+    public nombre:string = "David Cifuentes";
+    public edad:number = 20
+    public mayorEdad:boolean = true;
+    public trabajos:Array<string> = ['Carpintero', 'Alba;il', 'Fontanero'];
+    public trabajosAny:Array<any> = ['Carpintero', 44, 'Fontanero'];
+    comodin:any = "Cualquier Cosa"
 }
