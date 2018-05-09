09bf000b421689305716c545e4d85b32e5c25c41
DFCGamerYT <DFCGamerYT@gmail.com>
2018-05-09 07:23:26 -0500

    Clases, Modelo y Objetos

--- curso-angular/src/app/empleado/empleado.component.ts
+++ curso-angular/src/app/empleado/empleado.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Empleado } from './empleado';
 
 @Component({
     selector: 'empleado',
     templateUrl: './empleado.component.html'
 })
-export class EmpleadoComponent{
-    public titulo = "Listado empleados"
+export class EmpleadoComponent implements OnInit {
+    public titulo = 'Listado empleados';
+    public empleado: Empleado;
+    public trabajadores: Array<Empleado>;
+
+    constructor() {
+        this.empleado = new Empleado('David Cifuentes', 21, 'Programador', true);
+        this.trabajadores = [
+            new Empleado('David Cifuentes', 21, 'Programador', true),
+            new Empleado('Ana Lopez', 30, 'Cocinero', false),
+            new Empleado('Victor Martinez', 35, 'Ingeniero', true)
+        ];
+    }
+
+    ngOnInit() {
+        console.log(this.empleado);
+        console.log(this.trabajadores);
+    }
 }
