73285a64ff13528215cec2bf9ede9811cbbf8a64
DFCGamerYT <38484703+DFCGamerYT@users.noreply.github.com>
2018-08-09 10:16:43 -0500

    PIPES PARA FECHA

--- curso-angular/src/app/home/home.component.ts
+++ curso-angular/src/app/home/home.component.ts
@@ -11,14 +11,27 @@ import { RopaService } from '../services/ropa.service';
 
 export class HomeComponent{
     public titulo: string = "Pagina Principal";
-    public ropa: string;
+    public listado_ropa: Array<string>;
+    public prenda_a_guardar: string;
+    public fecha;
 
     constructor(
         private _ropaService: RopaService
-    ){}
+    ){
+        this.fecha = new Date(2018, 8, 8);
+    }
 
     ngOnInit(){
-        this.ropa = this._ropaService.prueba();
-        console.log(this.ropa);
+        this.listado_ropa = this._ropaService.getRopa();
+        console.log(this.listado_ropa)
+    }
+
+    guardarPrenda(){
+        this._ropaService.addRopa(this.prenda_a_guardar);
+        this.prenda_a_guardar = "";
+    }
+
+    eliminarPrenda(index:number){
+        this._ropaService.deleteRopa(index);
     }
 }
--- curso-angular/src/app/services/ropa.service.ts
+++ curso-angular/src/app/services/ropa.service.ts
@@ -3,7 +3,23 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class RopaService{
     public nombre_prenda: string = 'Pantalones Vaqueros';
+
+    public coleccion_ropa = ['Pantalones Blancos', 'Camiseta Roja']
     prueba(){
         return this.nombre_prenda;
+    }
+
+    addRopa(nombre_prenda){
+        this.coleccion_ropa.push(nombre_prenda);
+        return this.getRopa();
+    }
+
+    getRopa(){
+        return this.coleccion_ropa;
+    }
+
+    deleteRopa(index:number){
+        this.coleccion_ropa.splice(index, 1);
+        return this.getRopa();
     }
 }
